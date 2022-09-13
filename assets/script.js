93cf27f149f52c9c24fff4a1de7fc8ec58ee4807
Alex Hsieh <alexhsieh12@gmail.com>
2022-09-12 17:16:30 -0700

    push to check

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,56 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById('question')
-const answerButtonsElement = documents.getElementById('answer-buttons')
+const answerButtonsElement = document.getElementById('answer-buttons')
 startButton.addEventListener('click', startGame)
 
 
 function startGame() {
-    console.log('started')
     startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    shuffledQuestions = question.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innertext = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText= answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer) //why down here?
+        answerButtonsElement.appendChild(button)
+    })
 }
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
 function selectAnswer() {
       
 }
 
 const question = [
     {
-        questin: 'What is 2 + 2',
+        question: 'What is 2 + 2',
         answers: [
             { text: '4', correct: true  },
-            { text: '22', correct: false }        ]
+            { text: '22', correct: false }      
+        ]
     }
 ]
